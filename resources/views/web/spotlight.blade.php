544643af3acca73459fbddaf0aacf246446655be
lindomarc <lindomarc@outlook.com>
2021-05-11 19:41:04 -0300

    feat: Page 'Destaque'

---
+++ resources/views/web/spotlight.blade.php
@@ -0,0 +1,10 @@
+@extends('web.layout.master')
+@section('content')
+    <section>
+        <div class="container">
+            <h2 class="text-center p-5 bg-white text-front font-weight-bold">Destaque</h2>
+            <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad alias animi assumenda atque aut deleniti doloribus ducimus explicabo fuga harum ipsum maxime molestiae, molestias nemo neque nisi nobis obcaecati, officia possimus, quibusdam ratione sequi voluptatibus. Amet corporis culpa cumque deleniti dolore incidunt magnam magni, minima nulla perspiciatis quas quis, recusandae saepe. Accusantium architecto asperiores aspernatur commodi cum cupiditate deserunt doloribus esse, est expedita fuga illo iure magnam necessitatibus officia omnis pariatur, perspiciatis placeat quos recusandae sequi, similique totam. Architecto asperiores nemo perferendis provident quasi. Animi beatae cupiditate delectus error facilis id laboriosam porro qui quibusdam! Ab expedita explicabo non sint.</p>
+        </div>
+    </section>
+
+@endsection
