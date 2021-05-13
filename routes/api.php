7288b750d33d005400147e8f016e1b27ad4aa184
lindomarc <lindomarc@outlook.com>
2021-05-12 23:10:56 -0300

    feat: Protecting API routes

--- routes/api.php
+++ routes/api.php
@@ -21,10 +21,11 @@ use Illuminate\Support\Facades\Route;
 Route::group(['namespace' => 'Api', 'as' => 'api.'],function (){
 
     Route::post('/auth/login', 'AuthController@login')->name('login');
-    Route::post('/auth/logout', 'AuthController@logout')->name('logout');
-    
-    Route::post('/me', 'AuthController@me')->name('me');
 
-    Route::apiResource('companies', 'CompanyController');
+    Route::group(['middleware'=> ['apiJwt']],function (){
+        Route::post('/auth/logout', 'AuthController@logout')->name('logout');
+        Route::post('/me', 'AuthController@me')->name('me');
+        Route::apiResource('companies', 'CompanyController');
+    });
 
 });
