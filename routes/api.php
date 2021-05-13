6ffab08d5c47d1925f3c5d1272f4f17db53db38e
lindomarc <lindomarc@outlook.com>
2021-05-12 22:40:06 -0300

    feat: Instaling Component JWT

--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(['namespace' => 'Api', 'as' => 'api.'],function (){
 
+    Route::post('/auth/login', 'AuthController@login')->name('login');
+    Route::post('/auth/logout', 'AuthController@logout')->name('logout');
+    
+    Route::post('/me', 'AuthController@me')->name('me');
+
     Route::apiResource('companies', 'CompanyController');
 
 });
