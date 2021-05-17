6dfdbda1be6318293469fa4e7d95d2b21974625a
lindomarc <lindomarc@outlook.com>
2021-05-17 11:49:30 -0300

    fix: add migrate properties.views

---
+++ database/migrations/2021_05_17_144622_add_properties_table_add_count_views.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddPropertiesTableAddCountViews extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('properties', function (Blueprint $table) {
+            $table->unsignedBigInteger('views')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('properties', function (Blueprint $table) {
+            //
+        });
+    }
+}
