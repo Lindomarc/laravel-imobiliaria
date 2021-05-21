936b83047a67daf6059d91c812c172795a488567
lindomarc <lindomarc@outlook.com>
2021-05-21 15:27:23 -0300

    feat: add the ...HasRoles trait to User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,7 +12,7 @@ use Tymon\JWTAuth\Contracts\JWTSubject;
 
 class User extends Authenticatable implements JWTSubject
 {
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasRoles;
 
     /**
      * The attributes that are mass assignable.
